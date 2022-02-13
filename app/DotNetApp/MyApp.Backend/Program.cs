9f3d341dffae01846ced292cd0eba4b52cd56ae6
Alexander Lisitsyn <lisitsyn.alex@gmail.com>
2022-02-13 13:21:18 +0300

    Database migrations

--- app/DotNetApp/MyApp.Backend/Program.cs
+++ app/DotNetApp/MyApp.Backend/Program.cs
@@ -4,12 +4,13 @@ using MySqlConnector;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient(_ => new MySqlConnection(builder.Configuration["ConnectionStrings:Default"]));
+
+var connectionString = $"{builder.Configuration["ConnectionString"]};database={builder.Configuration["DbName"]}";
+builder.Services.AddTransient(_ => new MySqlConnection(connectionString));
 
 var app = builder.Build();
 
--- app/DotNetApp/MyApp.DbMigrator/Program.cs
+++ app/DotNetApp/MyApp.DbMigrator/Program.cs
@@ -3,23 +3,23 @@ using MySqlConnector;
 
 // Конфиг.
 var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.Development.json")
     .AddEnvironmentVariables()
     .Build();
 
-var connectionString = config.GetValue<string>("ConnectionStrings:Default");
+var connectionString = config.GetValue<string>("ConnectionString");
 var dbName = config.GetValue<string>("DbName");
 
-// Коннекшн.
-
+Console.WriteLine($"ConnectionString: {connectionString}, DbName: {dbName}");
 
 // Создание БД.
 using var connDb = new MySqlConnection(connectionString);
 connDb.Open();
 
+Console.WriteLine("CREATING DB - START");
 using var createDbCommand = connDb.CreateCommand();
-createDbCommand.CommandText = $"CREATE DATABASE IF NOT EXISTS `${dbName}`;";
+createDbCommand.CommandText = $"CREATE DATABASE IF NOT EXISTS `{dbName}`;";
 createDbCommand.ExecuteNonQuery();
+Console.WriteLine("CREATING DB - FINISH");
 
 connDb.Close();
 
@@ -27,12 +27,14 @@ connDb.Close();
 using var connTable = new MySqlConnection($"{connectionString};database={dbName}");
 connTable.Open();
 
+Console.WriteLine("CREATING TABLES - START");
 using var createTableCommand = connTable.CreateCommand();
 createTableCommand.CommandText = @"create table users
     (id INT NOT NULL AUTO_INCREMENT,    
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)    );";
 createTableCommand.ExecuteNonQuery();
+Console.WriteLine("CREATING TABLES - FINISH");
 
 connTable.Close();
 
